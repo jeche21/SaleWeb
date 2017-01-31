5f3f9a81a6f550ff7b43b188d73274832ca4f786
jeche21 <jesus-alcorcon@hotmail.com>
2017-01-31 14:19:57 +0100

    prueba

---
+++ Application.java
@@ -0,0 +1,16 @@
+package es.sidelab.EjerciciosDAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+			SpringApplication.run(Application.class, args);
+	}
+}
