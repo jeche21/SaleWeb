9ac3a5fb0aa5a1ba46084b6e568fcda348a7033d
agala5 <agalanez5@gmail.com>
2017-02-09 12:29:08 +0100

    Creacion de la entidad Comentario con el repositorio correspondiente. Se ha modificado el pom.xml y se ha hecho un Maven>clean y un Maven>install para solucionar problemas de ejecucion.

--- src/main/java/es/sidelab/SaleWeb/Comentario.java
+++ src/main/java/es/sidelab/SaleWeb/Comentario.java
@@ -1,5 +1,59 @@
 package es.sidelab.SaleWeb;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Comentario {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String cuerpo;
+	private String fecha;
+	
+	@OneToOne
+	private Usuario autor;
+	
+	
+	protected Comentario(){
+		//Sirve para que SpringData pueda instanciar el objeto 
+	}
+	
+	public Comentario (String cuerpo, String fecha, Usuario autor){
+		this.cuerpo = cuerpo;
+		this.fecha = fecha;
+		this.autor = autor;
+	}
+
+	public String getCuerpo() {
+		return cuerpo;
+	}
+
+	public void setCuerpo(String cuerpo) {
+		this.cuerpo = cuerpo;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public Usuario getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Usuario autor) {
+		this.autor = autor;
+	}
+	
+	
 
 }
---
+++ src/main/java/es/sidelab/SaleWeb/ComentarioRepository.java
@@ -0,0 +1,7 @@
+package es.sidelab.SaleWeb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ComentarioRepository extends JpaRepository<Comentario, Long>{
+
+}
