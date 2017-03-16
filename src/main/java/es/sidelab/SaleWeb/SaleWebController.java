df1faaef87f17612f6beabd70242b5dd2efc53e6
g4l4 <agalanez5@gmail.com>
2017-03-16 11:23:11 +0100

    Limpieza de codigo

--- src/main/java/es/sidelab/SaleWeb/SaleWebController.java
+++ src/main/java/es/sidelab/SaleWeb/SaleWebController.java
@@ -1,5 +1,4 @@
 package es.sidelab.SaleWeb;
-import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +15,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import es.sidelab.SaleWeb.Pedido;
 import es.sidelab.SaleWeb.PedidoRepository;
@@ -51,10 +49,12 @@ public class SaleWebController {
 		return "principal";
 	}
 	
+	
 	@GetMapping("/loggin")
 	public String logginUsuario(){
 		return "loggin_usuario";
 	}
+	
 	
 	@GetMapping("/tienda")
 	public String tienda (Model model,HttpServletRequest request, HttpSession sesion){
@@ -67,7 +67,7 @@ public class SaleWebController {
 		return "tienda";
 	}
 	
-	//*** DONE ***
+	
 	@PostMapping("/articulo/nuevo")
 	public String nuevoArticulo(Model model, Articulo articulo) {
 	
@@ -85,172 +85,183 @@ public class SaleWebController {
 		return "articulo_guardado";
 	}
 	
-	//*** DONE ***
-		@GetMapping("/articulo/{id}")
-		public String verArticulo (Model model, @PathVariable long id,HttpServletRequest request/*@PathVariable int num*/){
-			
-			Articulo articulo = articulo_repository.findOne(id);
-			model.addAttribute("articulo", articulo);
-			//Articulo articulo_guardado = articulo_repository.findOne(id);
-			//model.addAttribute("articulo",articulo_guardado);
-			model.addAttribute("comentarios",comentario_repository.findByArticulo(articulo));
-			model.addAttribute("admin",request.isUserInRole("ADMIN"));
-			
-			return "ver_articulo";
-		}
+	
+	@GetMapping("/articulo/{id}")
+	public String verArticulo (Model model, @PathVariable long id,HttpServletRequest request){
 		
-		@GetMapping("/articulo/{id}/eliminar")
-		public String eliminarArticulo (Model model, @PathVariable long id /*@PathVariable int num*/){
-			
-			Articulo articulo = articulo_repository.findOne(id);
-			articulo_repository.delete(articulo);
-			model.addAttribute("articulos", articulo_repository.findAll());
-			return "articulo_eliminado";
-		}
+		Articulo articulo = articulo_repository.findOne(id);
+		model.addAttribute("articulo", articulo);
+		model.addAttribute("comentarios",comentario_repository.findByArticulo(articulo));
+		model.addAttribute("admin",request.isUserInRole("ADMIN"));
 		
-		//*** DONE ***
-		@GetMapping("/carrito")
-		public String verCarrito (Model model, HttpSession sesion){
-			Usuario usuario = usuario_repository.findByEmail((String) sesion.getAttribute("email"));
-			model.addAttribute("articulos_carrito",  usuario.getCarrito().getArticulosCarrito());
-			return "carrito";
-		}
+		return "ver_articulo";
+	}
+	
+	
+	@GetMapping("/articulo/{id}/eliminar")
+	public String eliminarArticulo (Model model, @PathVariable long id){
 		
-		//*** DONE ***
-		@GetMapping("/carrito/{num}")
-		public String verArticuloCarrito (Model model, @PathVariable int num, HttpSession sesion){
-			Usuario usuario = usuario_repository.findByEmail((String) sesion.getAttribute("email"));
-			model.addAttribute("articulo_carrito", usuario.getCarrito().getArticulosCarrito().get(num-1));
-			
-			return "ver_articuloCarrito";
-		}
+		Articulo articulo = articulo_repository.findOne(id);
+		articulo_repository.delete(articulo);
+		model.addAttribute("articulos", articulo_repository.findAll());
+		return "articulo_eliminado";
+	}
+	
+
+	@GetMapping("/carrito")
+	public String verCarrito (Model model, HttpSession sesion){
 		
+		Usuario usuario = usuario_repository.findByEmail((String) sesion.getAttribute("email"));
+		model.addAttribute("articulos_carrito",  usuario.getCarrito().getArticulosCarrito());
+		return "carrito";
+	}
+	
+
+	@GetMapping("/carrito/{num}")
+	public String verArticuloCarrito (Model model, @PathVariable int num, HttpSession sesion){
 		
-		//*** DONE ***
-		@GetMapping("/articulo/{id}/añadido")
-		public String añadirArticulo (Model model, @PathVariable long id, HttpSession sesion /*@PathVariable int num*/){
-			String resultado = "";
-			Articulo articulo = articulo_repository.findOne(id);
-			//articulos_carrito.add(articulo);
-			//articulos.remove(num-1);
-			
-			//Articulo articulo_guardado = articulo_repository.findOne(id);
-			Usuario usuario = usuario_repository.findByEmail((String) sesion.getAttribute("email"));
-			Carrito carritoUsuario = usuario.getCarrito();
-			articulo.getArticulosEnCarrito().add(carritoUsuario);
-			int cantidad = articulo.getCantidad();
-			if (cantidad == 0){
-				resultado = "producto_no_disponible";
-			}else{
-				cantidad--;
-				articulo.setCantidad(cantidad);
-				articulo_repository.save(articulo);
-				resultado = "articulo_añadido";
-			}
-			return resultado;
-		}
+		Usuario usuario = usuario_repository.findByEmail((String) sesion.getAttribute("email"));
+		model.addAttribute("articulo_carrito", usuario.getCarrito().getArticulosCarrito().get(num-1));
 		
-		//*** DONE ***
-		@GetMapping("/carrito/{num}/eliminado")
-		public String eliminarArticuloCarrito (Model model,/*@PathVariable long id*/ @PathVariable int num, HttpSession sesion){
-			//articulos_carrito.remove(num-1);
-			Usuario usuarioBuscado = usuario_repository.findByEmail((String) sesion.getAttribute("email"));
-			Carrito usuario = usuarioBuscado.getCarrito();
-			Articulo articulo = usuario.getArticulosCarrito().get(num-1);
-			articulo.getArticulosEnCarrito().remove(usuario);
-			//LINEAS NUEVAS
-			int cantidad = articulo.getCantidad();
-			cantidad++;
+		return "ver_articuloCarrito";
+	}
+	
+	
+	@GetMapping("/articulo/{id}/añadido")
+	public String añadirArticulo (Model model, @PathVariable long id, HttpSession sesion){
+		
+		String resultado = "";
+		Articulo articulo = articulo_repository.findOne(id);
+
+		Usuario usuario = usuario_repository.findByEmail((String) sesion.getAttribute("email"));
+		Carrito carritoUsuario = usuario.getCarrito();
+		articulo.getArticulosEnCarrito().add(carritoUsuario);
+		int cantidad = articulo.getCantidad();
+		if (cantidad == 0){
+			resultado = "producto_no_disponible";
+		}else{
+			cantidad--;
 			articulo.setCantidad(cantidad);
 			articulo_repository.save(articulo);
-			return "articuloCarritoEliminado";
+			resultado = "articulo_añadido";
 		}
+		return resultado;
+	}
+	
+	
+	@GetMapping("/carrito/{num}/eliminado")
+	public String eliminarArticuloCarrito (Model model, @PathVariable int num, HttpSession sesion){
+
+		Usuario usuarioBuscado = usuario_repository.findByEmail((String) sesion.getAttribute("email"));
+		Carrito usuario = usuarioBuscado.getCarrito();
+		Articulo articulo = usuario.getArticulosCarrito().get(num-1);
+		articulo.getArticulosEnCarrito().remove(usuario);
+
+		int cantidad = articulo.getCantidad();
+		cantidad++;
+		articulo.setCantidad(cantidad);
+		articulo_repository.save(articulo);
+		return "articuloCarritoEliminado";
+	}
+	
+	
+	@PostMapping("/usuario/nuevo")
+	public String UsuarioNuevo (Model model, Usuario usuario, HttpSession sesion){
 		
-		@PostMapping("/usuario/nuevo")
-		public String UsuarioNuevo (Model model, Usuario usuario, HttpSession sesion){
-			//Guardo el usuario creado
-			sesion.setAttribute("email", usuario.getEmail());
-			List<String> rol = new ArrayList<String>();
-			rol.add("ROLE_USER");
-			usuario.setRol(rol);
-			//Puente ya que el constructor de la clase no me lo hace
-			Carrito carrito = new Carrito();
-			usuario.setCarrito(carrito);
-			String contraseña = usuario.getContraseña();
-			usuario.setContraseña(new BCryptPasswordEncoder().encode(contraseña));
-			usuario_repository.save(usuario);
-			return "usuario_registrado";
-		}
-			
-		@PostMapping("/comentario/articulo/{id}")
-		public String comentar (Model model, Comentario comentario, @PathVariable long id, HttpSession sesion){
-			//Guardo el comentario escrito creado
-			Articulo articulo = articulo_repository.findOne(id);
-			comentario.setArticulo(articulo);
-			Usuario usuario = usuario_repository.findByEmail((String) sesion.getAttribute("email"));
-			comentario.setAutor(usuario);
-			articulo.getComentarios().add(comentario);
-			comentario_repository.save(comentario);
-			return "comentario_guardado";
-		}
-		//tenemos que poner un boton eliminar al lado de cada comentario
-		@GetMapping("/comentario/{id}/eliminado")
-		public String comentarioEliminar (Model model,@PathVariable long id){
-			//Comentario comentarioSaved = comentario_repository.findOne(id);
-			comentario_repository.delete(id);
-			//comentarios.remove(comentarioSaved);
-			//model.addAttribute(comentario_repository.findAll());
-			return "comentario_eliminado";
-		}
-			
-		@GetMapping("/articulo/comentario/{id}")
-		public String verComentario(Model model, @PathVariable long id, HttpServletRequest request, HttpSession sesion){
-			Comentario comentario = comentario_repository.findOne(id);
-			Boolean permitoEliminar = ((request.isUserInRole("ROLE_ADMIN")) || 
-										(comentario.getAutor().getEmail().equals((String) sesion.getAttribute("email"))));
-			model.addAttribute("botonEliminar", permitoEliminar);
-			model.addAttribute("comentario", comentario);
-			return "ver_comentario";
-		}
-			
-		@PostMapping("/carrito/comprar")
-		public String comprarArticulos(Model model, HttpSession sesion){
-			Usuario usuario =  usuario_repository.findByEmail((String) sesion.getAttribute("email"));
-			Carrito carrito = usuario.getCarrito();
-			model.addAttribute("usuario",usuario);
-			model.addAttribute("articulos_carrito", carrito.getArticulosCarrito());
-			return "pedido";
-		}
-			
-		@PostMapping("/pedido")
-		public String pedido(Model model, HttpSession sesion){
-			Usuario usuario =  usuario_repository.findByEmail((String) sesion.getAttribute("email"));
-			List<Articulo> articulosPedido = articulo_repository.findByArticulosEnCarrito(usuario.getCarrito());
-			Pedido pedido = new Pedido();
-			pedido.setUsuario(usuario);
-			SocketCliente socket = new SocketCliente();
-			socket.enviarEmail(usuario.getEmail());
-			for(Articulo articulo: articulosPedido){
+		//Guardo el usuario creado
+		sesion.setAttribute("email", usuario.getEmail());
+		List<String> rol = new ArrayList<String>();
+		rol.add("ROLE_USER");
+		usuario.setRol(rol);
+		
+		//Puente ya que el constructor de la clase no me lo hace
+		Carrito carrito = new Carrito();
+		usuario.setCarrito(carrito);
+		String contraseña = usuario.getContraseña();
+		usuario.setContraseña(new BCryptPasswordEncoder().encode(contraseña));
+		usuario_repository.save(usuario);
+		return "usuario_registrado";
+	}
+		
+	
+	@PostMapping("/comentario/articulo/{id}")
+	public String comentar (Model model, Comentario comentario, @PathVariable long id, HttpSession sesion){
+		
+		//Guardo el comentario escrito creado
+		Articulo articulo = articulo_repository.findOne(id);
+		comentario.setArticulo(articulo);
+		Usuario usuario = usuario_repository.findByEmail((String) sesion.getAttribute("email"));
+		comentario.setAutor(usuario);
+		articulo.getComentarios().add(comentario);
+		comentario_repository.save(comentario);
+		return "comentario_guardado";
+	}
+
+	
+	@GetMapping("/comentario/{id}/eliminado")
+	public String comentarioEliminar (Model model,@PathVariable long id){
+		
+		comentario_repository.delete(id);
+		return "comentario_eliminado";
+	}
+		
+	
+	@GetMapping("/articulo/comentario/{id}")
+	public String verComentario(Model model, @PathVariable long id, HttpServletRequest request, HttpSession sesion){
+		
+		Comentario comentario = comentario_repository.findOne(id);
+		Boolean permitoEliminar = ((request.isUserInRole("ROLE_ADMIN")) || 
+									(comentario.getAutor().getEmail().equals((String) sesion.getAttribute("email"))));
+		model.addAttribute("botonEliminar", permitoEliminar);
+		model.addAttribute("comentario", comentario);
+		return "ver_comentario";
+	}
+		
+	
+	@PostMapping("/carrito/comprar")
+	public String comprarArticulos(Model model, HttpSession sesion){
+		
+		Usuario usuario =  usuario_repository.findByEmail((String) sesion.getAttribute("email"));
+		Carrito carrito = usuario.getCarrito();
+		model.addAttribute("usuario",usuario);
+		model.addAttribute("articulos_carrito", carrito.getArticulosCarrito());
+		return "pedido";
+	}
+		
+	
+	@PostMapping("/pedido")
+	public String pedido(Model model, HttpSession sesion){
+		
+		Usuario usuario =  usuario_repository.findByEmail((String) sesion.getAttribute("email"));
+		List<Articulo> articulosPedido = articulo_repository.findByArticulosEnCarrito(usuario.getCarrito());
+		Pedido pedido = new Pedido();
+		pedido.setUsuario(usuario);
+		SocketCliente socket = new SocketCliente();
+		socket.enviarEmail(usuario.getEmail());
+		
+		for(Articulo articulo: articulosPedido){
 			pedido.getArticulosComprados().add(articulo);
-			}
-			pedido_repository.save(pedido);
-				
-			Carrito carrito = usuario.getCarrito();
-			for(Articulo articulo: articulosPedido){
-				articulo.getArticulosEnCarrito().remove(carrito);
-				articulo_repository.save(articulo);
-			}
-				
-			return "pedido_realizado";
 		}
-		
-		@PostMapping("/registrar_usuario")
-		public String registrarUsuario(Model model){
-			return "nuevoUsuario";
+		pedido_repository.save(pedido);
+			
+		Carrito carrito = usuario.getCarrito();
+		for(Articulo articulo: articulosPedido){
+			articulo.getArticulosEnCarrito().remove(carrito);
+			articulo_repository.save(articulo);
 		}
-		
-		@PostMapping("/nuevo_articulo")
-		public String añadirArticulo(){
-			return "nuevoArticulo";
-		}
+			
+		return "pedido_realizado";
+	}
+	
+	
+	@PostMapping("/registrar_usuario")
+	public String registrarUsuario(Model model){
+		return "nuevoUsuario";
+	}
+	
+	
+	@PostMapping("/nuevo_articulo")
+	public String añadirArticulo(){
+		return "nuevoArticulo";
+	}
 }
