5a0b0506a2bb75e8272afcecd06ddedb1d946c53
cesar cuesta vera <cesarcuestavera@yahoo.es>
2017-04-06 12:48:05 +0200

    Añado dependencias de hazelcast y configuro aplicacion.java para que funcione hazelcast

--- src/main/java/es/sidelab/SaleWeb/Application.java
+++ src/main/java/es/sidelab/SaleWeb/Application.java
@@ -1,7 +1,13 @@
 package es.sidelab.SaleWeb;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +15,12 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	@Bean
+	 public Config config() {
+	 Config config = new Config();
+	 JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+	 joinConfig.getMulticastConfig().setEnabled(false);
+	 joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList("127.0.0.1"));
+	 return config;
+	 }
 }
